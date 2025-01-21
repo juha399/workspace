2fb327093587b49d2d5805170e61539c3786883a
juha399 <awe9224@naver.com>
2025-01-22 06:52:59 +0900

    FEAT: creatRef 사용

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';  
-import App from './App';  
+import App from './App';   
 
+class RefSample extends Component{
+    input= React.createRef();
+}
+
+handleFocus = () => {
+    this. input.current.focus();
+}
 ReactDOM.render(
     <React.StrictMode>
         <App />
     </React.StrictMode>,
     document.getElementById('root')  
 );
+ 
